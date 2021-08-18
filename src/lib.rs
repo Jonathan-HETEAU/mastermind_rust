fc89747112f6dcfc2327136ba79376984cedb78f
Heteau Jonathan <jonathan.heteau@gmail.com>
2021-08-18 17:16:35 +0200

    change struct to type

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ impl Color {
             Color::Green => 5,
         }
     }
-    pub fn from_value (value: usize) -> Color {
+    pub fn from_value(value: usize) -> Color {
         match value {
             0 => Color::Black,
             1 => Color::White,
@@ -35,33 +35,30 @@ impl Color {
     }
 }
 
-#[derive(Clone, Copy)]
-pub struct Code([Color; CODE_SIZE as usize]);
+pub type Code = [Color; CODE_SIZE as usize];
 
-impl Code {
-    pub fn evaluate(&self, code: &Code) -> (u8, u8) {
-        let mut leftover_self: [u8; COLORS_NBR] = [0; COLORS_NBR];
-        let mut leftover_code: [u8; COLORS_NBR] = [0; COLORS_NBR];
+pub fn evaluate(source: &Code, code: &Code) -> (u8, u8) {
+    let mut leftover_self: [u8; COLORS_NBR] = [0; COLORS_NBR];
+    let mut leftover_code: [u8; COLORS_NBR] = [0; COLORS_NBR];
 
-        let mut good: u8 = 0;
-        let mut bad: u8 = 0;
+    let mut good: u8 = 0;
+    let mut bad: u8 = 0;
 
-        for i in 0..(CODE_SIZE as usize) {
-            let color_self = self.0[i];
-            let color_code = code.0[i];
-            if color_self == color_code {
-                good += 1;
-            } else {
-                leftover_self[color_self.value()] += 1;
-                leftover_code[color_code.value()] += 1;
-            }
+    for i in 0..(CODE_SIZE as usize) {
+        let color_self = source[i];
+        let color_code = code[i];
+        if color_self == color_code {
+            good += 1;
+        } else {
+            leftover_self[color_self.value()] += 1;
+            leftover_code[color_code.value()] += 1;
         }
-        for i in 0..COLORS_NBR {
-            bad += u8::min(leftover_self[i], leftover_code[i]);
-        }
-
-        (good, bad)
     }
+    for i in 0..COLORS_NBR {
+        bad += u8::min(leftover_self[i], leftover_code[i]);
+    }
+
+    (good, bad)
 }
 
 #[derive(Clone, Copy)]
@@ -90,12 +87,12 @@ impl Game {
     pub fn new() -> Self {
         Game {
             state: State::Playable(Playable { tries: Vec::new() }),
-            secret_code: Code([
+            secret_code: [
                 Color::from_value(rand::random::<usize>() % COLORS_NBR),
                 Color::from_value(rand::random::<usize>() % COLORS_NBR),
                 Color::from_value(rand::random::<usize>() % COLORS_NBR),
                 Color::from_value(rand::random::<usize>() % COLORS_NBR),
-            ]),
+            ],
         }
     }
 
@@ -113,7 +110,7 @@ impl Game {
     pub fn play(&mut self, code: Code) -> &State {
         match &mut self.state {
             State::Playable(playable) => {
-                let result = self.secret_code.evaluate(&code);
+                let result = evaluate(&self.secret_code, &code);
                 playable.tries.push(Try(code, result));
                 match (result, playable.tries.len()) {
                     ((CODE_SIZE, _), _) => {
@@ -144,88 +141,55 @@ mod tests_play {
     use crate::*;
     #[test]
     fn win() {
-        let code = Code([
+        let code = [
             Color::from_value(rand::random::<usize>() % COLORS_NBR),
             Color::from_value(rand::random::<usize>() % COLORS_NBR),
             Color::from_value(rand::random::<usize>() % COLORS_NBR),
             Color::from_value(rand::random::<usize>() % COLORS_NBR),
-        ]);
+        ];
 
         let mut game = Game::new_with_secret_code(code);
         let state = game.play(code);
         match state {
-            State::Finish(Finish(_,Result::Win,_))=> assert!(true),
+            State::Finish(Finish(_, Result::Win, _)) => assert!(true),
             _ => panic!(),
         }
     }
     #[test]
     fn faux() {
-        let mut game = Game::new_with_secret_code(Code([
-            Color::White,
-            Color::White,
-            Color::White,
-            Color::White,
-        ]));
-        let state = game.play(Code([
-            Color::Black,
-            Color::Black,
-            Color::Black,
-            Color::Black,
-        ]));
+        let mut game =
+            Game::new_with_secret_code([Color::White, Color::White, Color::White, Color::White]);
+        let state = game.play([Color::Black, Color::Black, Color::Black, Color::Black]);
         match state {
-            State::Playable(playable) => assert_eq!(playable.tries[0].1 ,(0,0)),
+            State::Playable(playable) => assert_eq!(playable.tries[0].1, (0, 0)),
             _ => panic!(),
         }
     }
 
     #[test]
-    fn mal_placer(){
-        let mut game = Game::new_with_secret_code(Code([
-            Color::White,
-            Color::Blue,
-            Color::Green,
-            Color::Black,
-        ]));
-        let state = game.play(Code([
-            Color::Black,
-            Color::White,
-            Color::Blue,
-            Color::Green,
-        ]));
+    fn mal_placer() {
+        let mut game =
+            Game::new_with_secret_code([Color::White, Color::Blue, Color::Green, Color::Black]);
+        let state = game.play([Color::Black, Color::White, Color::Blue, Color::Green]);
         match state {
-            State::Playable(playable) => assert_eq!(playable.tries[0].1 ,(0,4)),
+            State::Playable(playable) => assert_eq!(playable.tries[0].1, (0, 4)),
             _ => panic!(),
         }
     }
 
     #[test]
-    fn loose(){
-        let mut game = Game::new_with_secret_code(Code([
-            Color::White,
-            Color::Blue,
-            Color::Green,
-            Color::Black,
-        ]));
-        let mut state = game.play(Code([
-            Color::Black,
-            Color::White,
-            Color::Blue,
-            Color::Green,
-        ]));
-        
-        for _ in 0..9{
-            state = game.play(Code([
-                Color::Black,
-                Color::White,
-                Color::Blue,
-                Color::Green,
-            ]));
+    fn loose() {
+        let mut game =
+            Game::new_with_secret_code([Color::White, Color::Blue, Color::Green, Color::Black]);
+        let mut state = game.play([Color::Black, Color::White, Color::Blue, Color::Green]);
+
+        for _ in 0..9 {
+            state = game.play([Color::Black, Color::White, Color::Blue, Color::Green]);
         }
 
         match state {
-            State::Finish(Finish(_,Result::Loose,_))=> assert!(true),
+            State::Finish(Finish(_, Result::Loose, _)) => assert!(true),
             _ => panic!(),
         }
     }
-
 }
